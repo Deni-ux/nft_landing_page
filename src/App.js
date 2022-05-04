02c80af0312864e2bcebc143bb76fcfa0a11c18e
Denisa Haka <denisa.haka@ymail.com>
2022-05-04 13:42:34 +0100

    added technologies and source code

--- src/App.js
+++ src/App.js
@@ -11,12 +11,30 @@ const App = () => {
         mockupImg={assets.homeHero}
         banner="banner"
       />
+
       <SectionWrapper
         title="Best User Interface Marketplace"
         description="A reliable, easy and inviting UI of ProNef NFT Marketplace "
         mockupImg={assets.homeCards}
         reverse
       />
+
+      <Features />
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo, which runs natively on all users' devices. Easier than ever to get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Inovative store showcase "
+        description="The app contains two main screens. One that presents you a list of the NFTs and a second screen for detailed info in real time for each NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+
+      <Download />
     </>
   );
 };
--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,8 +1,29 @@
-﻿import React from 'react'
+﻿import React from 'react';
+
+import styles from '../styles/Global';
+import assets from '../assets';
+
+
 
 const Download = () => {
   return (
-    <div>Download</div>
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}> 
+           
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+        </div>
+
+        <button className={styles.btnPrimary}>Source code</button>
+        
+        <div className={styles.flexCenter}>
+          <img src={assets.scene} alt='full app scene' className={styles.fullImg} />
+      </div>
+
+      </div>
+
+      </div>
   )
 }
 
