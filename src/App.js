843dd8daccfd32d71da22653cea2bd1c3cca6e86
Denisa Haka <denisa.haka@ymail.com>
2022-05-04 13:51:53 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Download, Features, SectionWrapper } from "./components";
 import assets from "./assets";
+import styles from "./styles/Global";
 
 const App = () => {
   return (
@@ -35,6 +36,12 @@ const App = () => {
       />
 
       <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>An exciting learning and building process by {" "}
+          <span className="bold">Denisa Haka</span>
+        </p>
+      </div>
     </>
   );
 };
